65cd359903d5de691322d89d827e3b86bad2c86b
swknjj <sinwook9255@gmail.com>
2023-03-29 14:07:22 +0900

    예제2

--- 2023.03.28.sql
+++ 2023.03.28.sql
@@ -319,3 +319,16 @@ select * from emp where sal=(select max(sal) from emp);
 select * from emp where sal>(select sal from emp where ename = 'allen')
 	order by sal asc;
     
+    /*
+	연습문제 
+    1. clark 보다 늦게 입사한 사원 조회 
+    2. 부서번호가 20인 사원 중에서 전체 사원 평균 급여보다 높은 급여를 받는 사원 조회 
+    3. 2번 조회 결과에서 부서이름, 부서위치도 함께 조회 
+*/
+select * from emp where hiredate > ( select hiredate from emp where ename = 'clark' );
+
+select * from emp where deptno = '20' and sal>(select avg(sal) from emp);
+
+select * from emp e , dept d 
+	where e.deptno = '20' and d.deptno = '20' 
+		and e.sal>(select avg(sal) from emp);
